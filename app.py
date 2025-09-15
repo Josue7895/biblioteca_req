4f89eae9fe5b1f33ce2b79479c5f591169221bda
Josue7895 <acarapijose73@gmail.com>
2025-09-15 18:57:06 -0400

    Corregido app.py para Render con gunicorn

--- app.py
+++ app.py
@@ -28,5 +28,4 @@ def upload_file():
 def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# 👇 Eliminar app.run() porque Gunicorn se encarga en Render
